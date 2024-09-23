c190e04ad977051dda9572a4ad6bee6359eda1d5
jose.paulo <jose.paulo@ceuma.br>
2024-09-23 16:04:42 -0300

    js dos preset-button

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useState } from 'react';
 import './Popup.css';
 
 const Popup = () => {
+  const [endTime, setEndTime] = useState('');
+
+  const adjustTimeBy = (minutesToAdd) => {
+    const now = new Date();
+    now.setMinutes(now.getMinutes() + minutesToAdd);
+
+    const formattedTime = now.toTimeString().slice(0, 5);
+    setEndTime(formattedTime);
+  };
+
+  const sendMessageToContentScript = () => {
+    chrome.runtime.sendMessage({ action: 'setTimer', time: endTime }, (response) => {
+      console.log('Response from content script:', response);
+    });
+  };
+
   return (
     <div className="App">
-      <div class="container">
-        <p class="description">Set a time to end the Google Meet call</p>
+      <div className="container">
+        <p className="description">Set a time to end the Google Meet call</p>
 
-        <div class="input-group">
-          <input type="time" id="end-time-input" placeholder="HH:MM" />
-          <div class="preset-buttons">
-            <button id="set-30-mins">19:30</button>
-            <button id="set-45-mins">19:45</button>
-            <button id="set-1-hour">20:00</button>
+        <div className="input-group">
+          <input
+            type="time"
+            id="end-time-input"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+            placeholder="HH:MM"
+          />
+          <div className="preset-buttons">
+            <button onClick={() => adjustTimeBy(15)}>+15 minutos</button>
+            <button onClick={() => adjustTimeBy(30)}>+30 minutos</button>
+            <button onClick={() => adjustTimeBy(45)}>+45 minutos</button>
           </div>
         </div>
-        <button id="set-timer">Start</button>
+        <button id="set-timer" onClick={sendMessageToContentScript}>Start</button>
       </div>
       <hr />
-      <div class="footer-container">
+      <div className="footer-container">
         <span>Meet Timer</span>
         <p>Version 1.0</p>
       </div>
